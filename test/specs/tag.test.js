49d329842cf2e87b0de003bcba11a307a8bbf2cd
John Nolette <john@neetgroup.net>
2018-06-09 18:48:40 -0400

    Re-enabled tests

--- test/specs/tag.test.js
+++ test/specs/tag.test.js
@@ -8,29 +8,29 @@ describe('r-localize localize tag', function() {
     document.body.appendChild(document.createElement('tag'))
   })
 
-  // it('will properly localize top level item', function() {
-  //   riot.tag('tag', MOCK.tags.top)
-  //   riot.mount('tag')
-  //   expect(document.querySelector('h1').textContent).toBe(localize.localizations['en-US'].header)
-  // })
+  it('will properly localize top level item', function() {
+    riot.tag('tag', MOCK.tags.top)
+    riot.mount('tag')
+    expect(document.querySelector('h1').textContent).toBe(localize.localizations['en-US'].header)
+  })
 
-  // it('will properly localize multi level items', function() {
-  //   riot.tag('tag', MOCK.tags.deep)
-  //   riot.mount('tag')
-  //   expect(document.querySelector('h1').textContent).toBe(localize.localizations['en-US'].menu.help)
-  // })
+  it('will properly localize multi level items', function() {
+    riot.tag('tag', MOCK.tags.deep)
+    riot.mount('tag')
+    expect(document.querySelector('h1').textContent).toBe(localize.localizations['en-US'].menu.help)
+  })
 
-  // it('will properly localize attributes', function() {
-  //   riot.tag('tag', MOCK.tags.attribute)
-  //   riot.mount('tag')
-  //   expect(document.querySelector('h1').getAttribute('title')).toBe(localize.localizations['en-US'].header)
-  // })
+  it('will properly localize attributes', function() {
+    riot.tag('tag', MOCK.tags.attribute)
+    riot.mount('tag')
+    expect(document.querySelector('h1').getAttribute('title')).toBe(localize.localizations['en-US'].header)
+  })
 
-  // it('will properly localize items by locale', function() {
-  //   riot.tag('tag', MOCK.tags.locale)
-  //   riot.mount('tag')
-  //   expect(document.querySelector('h1').textContent).toBe(localize.localizations['es-SP'].header)
-  // })
+  it('will properly localize items by locale', function() {
+    riot.tag('tag', MOCK.tags.locale)
+    riot.mount('tag')
+    expect(document.querySelector('h1').textContent).toBe(localize.localizations['es-SP'].header)
+  })
 
   it('will properly update items', function(done) {
     riot.tag('tag', MOCK.tags.change)

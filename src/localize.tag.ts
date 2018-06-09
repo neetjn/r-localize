584d338939e9bac8cc5f048d96ee73fb92abb194
John Nolette <john@neetgroup.net>
2018-06-09 18:48:18 -0400

    Added new test for fallback content

--- src/localize.tag.ts
+++ src/localize.tag.ts
@@ -4,9 +4,9 @@ export function Tag (opts : object) {
   function localization () {
     const nodeContent = self.root.textContent || self.root.innerHTML
     // # fetch initial translation
-    let translation = self.opts.t == 'string' ? self.localize.translate(self.opts.t) : self.localize.translate(self.opts.t.i, self.opts.t.l)
+    let translation = typeof self.opts.t == 'string' ? self.localize.translate(self.opts.t) : self.localize.translate(self.opts.t.i, self.opts.t.l)
     // # if fallbackContent toggled and translation is the provided fallback, try to use existing node content
-    translation = this.localize.options.fallbackContent && translation == this.localize.options.fallback ? nodeContent ? nodeContent : translation : translation
+    translation = this.localize ? this.localize.options.fallbackContent && translation == this.localize.options.fallback ? nodeContent ? nodeContent : translation : translation : translation
     if (typeof self.opts.t == 'string')
       self.root.innerHTML = translation
     else

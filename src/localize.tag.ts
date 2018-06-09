0b2a346ad8af4a757fc68d966752db91efa91395
John Nolette <john@neetgroup.net>
2018-06-09 15:05:38 -0400

    Implemented requested functionality

--- src/localize.tag.ts
+++ src/localize.tag.ts
@@ -2,14 +2,18 @@ export function Tag (opts : object) {
   const self = this
   self.opts = opts
   function localization () {
+    const nodeContent = self.root.textContent || self.root.innerHTML
+    // # fetch initial translation
+    let translation = self.opts.t == 'string' ? self.localize.translate(self.opts.t) : self.localize.translate(self.opts.t.i, self.opts.t.l)
+    // # if fallbackContent toggled and translation is the provided fallback, try to use existing node content
+    translation = this.localize.options.fallbackContent && translation == this.localize.options.fallback ? nodeContent ? nodeContent : translation : translation
     if (typeof self.opts.t == 'string')
-      self.root.innerHTML = self.localize.translate(self.opts.t)
+      self.root.innerHTML = translation
     else
       if (!self.opts.t.attr)
-        self.root.innerHTML = self.localize.translate(self.opts.t.i, self.opts.t.l)
+        self.root.innerHTML = translation
       else
-        self.root.setAttribute(
-          self.opts.t.attr, self.localize.translate(self.opts.t.i, self.opts.t.l))
+        self.root.setAttribute(self.opts.t.attr, translation)
   }
   self.on('mount', localization)
   self.localize.on('updated', localization)

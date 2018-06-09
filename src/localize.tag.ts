c9e71dbda902c798d1870abafe76a94d431bd97c
John Nolette <john@neetgroup.net>
2018-06-09 18:58:43 -0400

    Cleaned up source

--- src/localize.tag.ts
+++ src/localize.tag.ts
@@ -4,9 +4,12 @@ export function Tag (opts : object) {
   function localization () {
     const nodeContent = self.root.textContent || self.root.innerHTML
     // # fetch initial translation
-    let translation = typeof self.opts.t == 'string' ? self.localize.translate(self.opts.t) : self.localize.translate(self.opts.t.i, self.opts.t.l)
+    let translation = typeof self.opts.t == 'string' ? self.localize.translate(
+      self.opts.t) : self.localize.translate(self.opts.t.i, self.opts.t.l)
     // # if fallbackContent toggled and translation is the provided fallback, try to use existing node content
-    translation = this.localize ? this.localize.options.fallbackContent && translation == this.localize.options.fallback ? nodeContent ? nodeContent : translation : translation : translation
+    if (this.localize)
+      if (this.localize.options.fallbackContent && translation == this.localize.options.fallback)
+        translation = nodeContent
     if (typeof self.opts.t == 'string')
       self.root.innerHTML = translation
     else
